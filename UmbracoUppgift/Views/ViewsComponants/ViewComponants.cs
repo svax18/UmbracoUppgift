a6ec32cdb7181d94fc8607def4db44e37338db64
Sven Axelsson <sven.axelsson1@learnet.se>
2024-12-18 10:01:50 +0100

    Add project files.

---
+++ UmbracoUppgift/Views/ViewsComponants/ViewComponants.cs
@@ -0,0 +1,20 @@
+﻿namespace UmbracoUppgift.Views.ViewsComponants
+{
+    public class View
+    {
+        
+    }
+
+
+
+
+
+    public class Media
+    {
+        public string Icon { get; set; }
+
+        public string MediaUrl { get; set; }
+    }
+}
+
+
